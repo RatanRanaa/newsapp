4366e2bcd13feb7f50bd0fd6ca390c1707ae9c52
Ratan Rana <ratanrana206@gmail.com>
2022-12-12 09:50:03 +0530

    responsive navbar

--- newsapp/src/components/Navbar.js
+++ newsapp/src/components/Navbar.js
@@ -23,17 +23,17 @@ const Navbar = (props) => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
+                    <li className="nav-item" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation">
                     <Link className="nav-link" aria-current="page" to="/">Home</Link>
                     </li>
                     {/* <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li> */}
-                    <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                    <li className="nav-item" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation"><Link className="nav-link" to="/business">Business</Link></li>
+                    <li className="nav-item" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                    <li className="nav-item" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation"><Link className="nav-link" to="/general">General</Link></li>
+                    <li className="nav-item" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation"><Link className="nav-link" to="/health">Health</Link></li>
+                    <li className="nav-item" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation"><Link className="nav-link" to="/science">Science</Link></li>
+                    <li className="nav-item" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation"><Link className="nav-link" to="/sports">Sports</Link></li>
+                    <li className="nav-item" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation"><Link className="nav-link" to="/technology">Technology</Link></li>
                 </ul>
                 </div>
                 <div className='abc'>
